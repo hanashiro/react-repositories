cd069384d3095e377d0edb9a3d66924c494e1ba1
hanashiro <felipeaah@gmail.com>
2022-10-29 16:45:14 -0300

    feat: Repository - GraphQL Query

---
+++ src/data/graphql/queries/repositoriesQuery.ts
@@ -0,0 +1,40 @@
+import { gql } from '@apollo/client';
+
+export const REPOSITORIES_SEARCH = gql`
+    query listRepositories(
+        $queryString: String!
+        $perPage: Int = 25
+        $cursor: String
+    ) {
+        search(
+            first: $perPage
+            query: $queryString
+            type: REPOSITORY
+            after: $cursor
+        ) {
+            repositoryCount
+            pageInfo {
+                startCursor
+                endCursor
+                hasNextPage
+                hasPreviousPage
+            }
+            edges {
+                node {
+                    ... on Repository {
+                        id
+                        name
+                        url
+                        forkCount
+                        stargazerCount
+                        owner {
+                            login
+                            url
+                            avatarUrl
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
